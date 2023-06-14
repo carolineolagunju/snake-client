e608b2d971de0bd4b479950eda91f150e4a0244b
carolineolagunju <olamidejr21@gmail.com>
2023-06-14 11:54:13 -0600

    implement connect function

---
+++ play.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+
+//establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '172.31.56.240',
+    port: 50541,
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding = ('utf8');
+
+
+  //listen to incoming data
+  conn.on('data', data => {
+    console.log('server says: ', data);
+  })
+
+
+  return conn;
+};
+
+console.log('Connecting ....');
+
+//calling function connect
+connect();
